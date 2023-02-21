b74ad71adad8e2b6d00aa3f02090bd6b0e74da79
nathantheperson <117259028+nathantheperson@users.noreply.github.com>
2023-02-21 03:57:02 -0500

    Update socket_http.h

--- socket_http.h
+++ socket_http.h
@@ -5,7 +5,7 @@
 int createSockHttp();
 
 int connectSockHttp(int socket_call, char* host, in_port_t port);
-
+ 
 int sendHttp(int socket_call, char* built_request);
 
 int recvHttp(int socket_call, char* server_reply);
